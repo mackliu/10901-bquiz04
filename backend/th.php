12b325c3c3787e73bbad9db9f69e0935022666b4
mackliu <macklun@gmail.com>
2020-07-30 15:52:05 +0800

    完成商品分類管理功能

--- backend/th.php
+++ backend/th.php
@@ -5,24 +5,49 @@
 <input type="text" name="mid_name" id="mid_name"><button onclick="addMid()">新增</button>
 
 </div>
-
+<table class="all type-list"></table>
 
 
 
 <script>
+getTypeList()
+getBigOption()
 
 function addBig(){
-    $.post("api/add_big.php",{'name':$("#big").val(),'parent':0},function(){
-        $.get("api/get_big.php",function(options){
-            $("#mid").html(options)
-        })
+    $.post("api/save_type.php",{'name':$("#big").val(),'parent':0},function(){
+        getBigOption()
+        getTypeList()
     })
 }
 
-
-function addMid(){
-
+function edit(id){
+    let newName=prompt("請輸入要修改的分類名稱",$("#t"+id).html())
+    if(newName!=null){
+        $("#t"+id).html(newName);
+        $.post("api/edit_type.php",{id,newName})
+    }
 }
 
+function getBigOption(){
+    $.get("api/get_big.php",function(options){
+            $("#mid").html(options)
+    })
+}
+
+function addMid(){
+    let name=$("#mid_name").val();
+    let big=$("#mid").val();
+
+    $.post("api/save_type.php",{'name':name,'parent':big},function(){
+        getBigOption()
+        getTypeList()
+    })
+}
+
+function getTypeList(){
+    $.get("api/get_type_list.php",function(list){
+        $(".type-list").html(list)
+    })
+}
 
 </script>
